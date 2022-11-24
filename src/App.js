70780f0d7dc854c202414432282071699161e5f0
catmasteryip <29121012+catmasteryip@users.noreply.github.com>
2022-11-24 15:59:14 +0800

    graphing done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {Component, PureComponent, useState, useEffect} from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Scatter } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  Title,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-function App () {
-  const [heartdata, setheartdata] = useState({
-    data: '',
-    heartsound: ''
-  });
-  useEffect(() => {
-    // for every second
-    const interval = setInterval(() => {
+// export const 
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      heartsound : '',
+      chartdict: {
+        datasets: [
+          {
+            label: 'A dataset',
+            data: Array.from({ length: 100 }, () => ({
+              x: 1,
+              y: 2,
+            })),
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+          },
+        ],
+      }
+    }
+    this.data = {
+      datasets: [
+        {
+          label: 'A dataset',
+          data: Array.from({ length: 100 }, () => ({
+            x: 1,
+            y: 2,
+          })),
+          backgroundColor: 'rgba(255, 99, 132, 1)',
+        },
+      ],
+    };
+    // this.chartdatadict = {
+    //   labels: [],
+    //   datasets: []
+    // }
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
       // get data from flask server
       fetch("/data", {
         method: 'GET',
@@ -23,58 +74,179 @@ function App () {
       })
       // read as json
       .then(res => {return res.json()})
-      .then(res => {console.log(res);return res;})
       // set to state
-      .then(res => {setheartdata(res);return res;})
-      // convert data byte string to float arr
+      
       .then(res => {
         var text = res['data']
         var arr = text.substring(1, text.length - 1).split(',').map(parseFloat);
-        var data = arr.map((value, index) => ({index, value}))
-        setheartdata({
-          data: data,
-          heartsound: res.heartsound
+        var data = Array.from({ length: 10 }, () => ({x: 1,y: 2}))
+        if (typeof(arr) !== 'undefined') {
+          data = arr.map((value, index) => ({x: index, y: value}));
+        }
+        // console.log(data);
+        this.setState({
+          data: arr,
+          heartsound: res.heartsound,
+          chartdict: {
+            // datasets: [
+            //   {
+            //     label: 'A dataset',
+            //     data: Array.from({ length: 100 }, () => ({
+            //       x: 1,
+            //       y: 2,
+            //     })),
+            //     backgroundColor: 'rgba(255, 99, 132, 1)',
+            //   },
+            // ],
+            datasets: [
+              {
+                label:'Heart Sound wave form',
+                data: data,
+                backgroundColor: 'rgba(255, 99, 132, 1)',
+              },
+            ],
+          }
+          // datasets: [
+            //   {
+            //     label:'Heart Sound wave form',
+            //     data: data,
+            //     backgroundColor: 'rgba(255, 99, 132, 1)',
+            //   },
+            // ],
+          
         })
-        console.log(data);
+        // console.log(this.state['data']);
+        // console.log(this.state['heartsound']);
+        console.log(this.state['chartdict']['datasets'][0]['data']);
+        console.log(this.data['datasets'][0]['data'].slice(0,11));
         return res;
         
       })
-      
-      // .then(res => {console.log()})
       .catch(error => console.log(error));
 
     }, 1000);
-    // clear interval
-    return () => clearInterval(interval);
-  }, []);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
   
-  return (
-
-    <div className="App">
-      
-    {/* </ResponsiveContainer> */}
-      {/* <p>{heartdata['data'].slice(0, 5)}</p> */}
-      <p>Presence of heart sound:</p>
-      <p>{heartdata['heartsound']}</p>
-      <p>Heart sound data:</p>
-      <ResponsiveContainer width='100%' height={50}>
-       <LineChart
-        width={'90%'}
-        height={'90%'}
-        data={heartdata['data']}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="index"/>
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
-      </LineChart>
-      </ResponsiveContainer>
-    </div>
-  )
+  render(){
+    return (
+    
+      <div className="App">
+        
+        
+        <p>Presence of heart sound:</p>
+        <p>{this.state['heartsound']}</p>
+        {/* debug use only
+        <p>Heart sound data:</p>
+        <p>{this.state['data'].slice(0, 5)}</p> */}
+        <p>Heart sound wave form</p>
+        <div className="chart-container">
+            <Scatter 
+            data={this.state['chartdict']}
+            options = {{
+              scales: {
+                x: {
+                  beginAtZero: true,
+                  max: 1000,
+                  min: 0
+                },
+                y: {
+                  beginAtZero: true,
+                  min: -500,
+                  max: 500
+                }
+              },
+              }}
+               />
+        </div>
+      </div>
+    )
+  }
 }
 export default App;
+// function App () {
+//   const [heartdata, setheartdata] = useState({
+//     data: [],
+//     heartsound: ''
+//   });
+//   const [chartdatadict, setchartdatadict] = useState({
+//     labels: [],
+//     datasets: []
+//   });
+  
+//   useEffect(() => {
+//     // for every second
+//     const interval = setInterval(() => {
+//       // get data from flask server
+//       fetch("/data", {
+//         method: 'GET',
+//         dataType: 'json',
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         }
+//       })
+//       // read as json
+//       .then(res => {return res.json()})
+//       // .then(res => {console.log(res);return res;})
+//       // set to state
+//       // .then(res => {setheartdata(res);return res;})
+//       // convert data byte string to float arr
+//       .then(res => {
+//         var text = res['data']
+//         var arr = text.substring(1, text.length - 1).split(',').map(parseFloat);
+        
+//         setheartdata({
+//           data: arr,
+//           heartsound: res.heartsound
+//         })
+//         console.log(res.heartsound);
+//         console.log(heartdata['heartsound']);
+//         return res;
+        
+//       })
+//       .then(res => {
+//         setchartdatadict({
+//           labels: Array.from(Array(heartdata['data'].length).keys()),
+//           datasets: [
+//             {
+//               label: 'Heart Sound wave form',
+//               data: heartdata['data'],
+//               borderColor: 'rgb(255, 99, 132)',
+//               backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//             }
+//           ]
+//         })
+
+//         // console.log(chartdatadict['labels'].slice(0, 5));
+//         return res;
+//     })
+//       .catch(error => console.log(error));
+
+//     }, 1000);
+//     // clear interval
+//     return () => clearInterval(interval);
+//   }, []);
+  
+//   return (
+    
+//     <div className="App">
+      
+      
+//       <p>Presence of heart sound:</p>
+//       <p>{heartdata['heartsound']}</p>
+//       {/* <p>Heart sound data:</p>
+//       <p>{heartdata['data'].slice(0, 5)}</p> */}
+//       <p>Heart sound wave form</p>
+//       <div className="chart-container">
+//           <Line options={options} data={chartdatadict} />;
+//       </div>
+//     </div>
+//   )
+// }
+
 
 
 // return (
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// ReactDOM.render(<App />, document.getElementById('root'))
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
